1a746ec2e4b473f498d23cb34c32c58465f504a4
LuisBarbosa02 <luiscastro250302@gmail.com>
2022-11-02 08:13:31 -0300

    Memory file created. Dict to store. Read and Write methods.

---
+++ memory_cs104.py
@@ -0,0 +1,21 @@
+Memory_Space = 256
+
+class Memory():
+  def __init__(self):
+    self.memory_dict = {}
+    self.init_memory_addresses()
+
+  def init_memory_addresses(self):
+    for i in range(Memory_Space):
+      self.memory_dict[f"{i:08b}"] = 0
+
+  def write(self, address, value):
+    if address in self.memory_dict:
+      self.memory_dict[address] = value
+      return
+    print("That's not a valid address!")
+  
+  def read(self, address):
+    if address in self.memory_dict:
+      return self.memory_dict[address]
+    print("That's not a valid address!")
